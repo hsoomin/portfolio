6886a187cb52447b8de8159254464fc6ad6e3fdc
hsoomin <wnghkdsns@naver.com>
2023-09-05 23:37:34 +0900

    수정

--- src/App.js
+++ src/App.js
@@ -1,13 +1,12 @@
 import React, { useState } from 'react';
 import './App.scss';
-// import Header from './components/Header';
 import Header from './components/Header';
 import Main from './components/Main';
 import { BrowserRouter, Route,Routes } from 'react-router-dom';
 // import Detail from './pages/Detail';
-import QuickButton from './components/QuickButton';
 import CustomCursor from './components/CustomCursor';
 import FollowCursor from './components/FollowCursor';
+import QuickButton from './components/QuickButton';
 
 function App() {
 
@@ -17,14 +16,15 @@ function App() {
   const handleToggleMode = () => {
     setIsDarkMode(!isDarkMode);
   };
+
+
   return (
     <BrowserRouter>
       <div className={`App ${isDarkMode ? 'dark' : 'light'}`}>
-        {/* <Header/> */}
         <Header onToggleMode={handleToggleMode} isDarkMode={isDarkMode}/>
-        <QuickButton />
         <CustomCursor />
         <FollowCursor /> 
+        <QuickButton />
           <Routes>
             <Route path='/' element={<Main/>}/>
             {/* <Route path='/detail/:id' element={<Detail />} /> */}
@@ -35,3 +35,5 @@ function App() {
 }
 
 export default App;
+
+
--- src/components/Cover.jsx
+++ src/components/Cover.jsx
@@ -30,3 +30,4 @@ const Cover = () => {
 };
 
 export default Cover;
+
